84595b8a72b4b1050df3752d09fd5f74cbfe4e03
theorye <ahaugsness@icloud.com>
2018-07-03 11:11:56 -0500

    added test sh through node

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('here in help');
+const exec = require('child_process').exec;
+var yourscript = exec('sh test.sh',
+        (error, stdout, stderr) => {
+            console.log(`${stdout}`);
+            console.log(`${stderr}`);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });
